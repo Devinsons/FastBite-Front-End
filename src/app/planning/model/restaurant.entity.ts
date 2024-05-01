57f02576ce4c7afbf8f7e4f5631de28befdda3ed
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-01 16:23:35 -0500

    feat: added API consumption service structure

---
+++ src/app/planning/model/restaurant.entity.ts
@@ -0,0 +1,19 @@
+export class Restaurant {
+  id:number;
+  name:string;
+  email:string;
+  phone:string;
+  address:string;
+  schedule:string;
+  urlToImage:string;
+
+  constructor() {
+    this.id = 0;
+    this.name = "";
+    this.email = "";
+    this.phone = "";
+    this.address = "";
+    this.schedule = "";
+    this.urlToImage = "";
+  }
+}

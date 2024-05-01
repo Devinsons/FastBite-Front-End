7b245d6774cdfdc981eeb1b3c7132130da5032b6
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-01 11:18:02 -0500

    feat: created sidebar schema and router

---
+++ src/app/execution/ejemplo-1/ejemplo-1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo-1',
+  templateUrl: './ejemplo-1.component.html',
+  styleUrl: './ejemplo-1.component.css'
+})
+export class Ejemplo1Component {
+
+}

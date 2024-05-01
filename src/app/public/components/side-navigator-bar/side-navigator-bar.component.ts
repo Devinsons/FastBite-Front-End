a6b514d93f0af48ad35dc62a8dfaa7514b9b23ed
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-01 11:23:37 -0500

    feat : added public componente

---
+++ src/app/public/components/side-navigator-bar/side-navigator-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side-navigator-bar',
+  templateUrl: './side-navigator-bar.component.html',
+  styleUrl: './side-navigator-bar.component.css'
+})
+export class SideNavigatorBarComponent {
+
+}

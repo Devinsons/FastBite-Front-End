0f2ad1de7af3c87132986fd2443c51ed48692828
RubenGenaro10 <rubengenaro10@gmail.com>
2024-05-02 00:36:18 -0500

    feat: completed logic for profile data update

--- src/app/profile/pages/profile-update/profile-update.component.ts
+++ src/app/profile/pages/profile-update/profile-update.component.ts
@@ -10,13 +10,15 @@ import {User} from "../../model/user.entity";
 export class ProfileUpdateComponent implements OnInit{
 
   user:User= {} as User;
+  uploadedImageSrc: string | undefined;
 
   constructor(private profileService: ProfileService){
   }
 
   ngOnInit(): void {
     console.log("Iniciando la perfil de usuario");
-    this.getProfileById(2)
+    this.getProfileById(1)
+
   }
 
   getProfileById(profileId: number) {
@@ -24,6 +26,8 @@ export class ProfileUpdateComponent implements OnInit{
       console.log("Response", response);
       this.user = response;
       console.log("User", this.user);
+      this.uploadedImageSrc = this.user.urlToImage;
+      console.log("Imagen", this.uploadedImageSrc);
     });
   }
 
@@ -34,4 +38,26 @@ export class ProfileUpdateComponent implements OnInit{
         console.log("Response", response);
       });
   }
+
+  /*Manejo de imagenes locales*/
+  onFileSelected(event:any){
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+
+      // Crear un FileReader para leer el contenido del archivo
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        // El resultado del FileReader será una URL de datos que representa el archivo
+        const dataUrl = reader.result;
+        if (typeof dataUrl === 'string') {
+          localStorage.setItem('uploadedImage', dataUrl);
+          this.uploadedImageSrc = dataUrl;
+        }
+      };
+      // Leer el contenido del archivo como una URL de datos
+      reader.readAsDataURL(file);
+    }
+  }
+
 }
